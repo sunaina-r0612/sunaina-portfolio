7a13804693bd0c75ee0d7630b701ca912cbfe0ba
sunaina-r0612 <sunainarustagi1101@gmail.com>
2024-02-29 19:30:04 +0530

    first commit

--- pages/about/index.js
+++ pages/about/index.js
@@ -239,7 +239,7 @@ const About = () => {
         </div>
         <div className="py-2 xl:py-6 flex flex-col gap-Y-2 xl:gap-Y-4 items-center xl:items-start">
           {aboutData[index].info.map((item, itemIndex) =>{
-            return(
+            (
               <div 
                 key={itemIndex}
                 className="flex-1 flex flex-col md:flex-row max-w-max gap-x-2 items-center text-white/60">
@@ -250,7 +250,7 @@ const About = () => {
                 </div>
                 <div className="flex gap-x-4">
                   {item.icons?.map((icon,itemIndex)=>{
-                    return <div className="text-xl">
+                    <div className="text-xl">
                       {icon}
                     </div>
                   })}
