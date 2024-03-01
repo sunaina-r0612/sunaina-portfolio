2ad511f50ee01253bfc9facbbc112b41a78dbf6f
sunaina-r0612 <sunainarustagi1101@gmail.com>
2024-03-01 12:56:58 +0530

    first commit

--- pages/about/index.js
+++ pages/about/index.js
@@ -226,7 +226,7 @@ const About = () => {
       </div>
       <div className="flex flex-col w-full xl:max-w-[48%] h-[480px]">
         <div className="flex gap-x-4 xl:gap-x-8 mx-auto xl:mx-0 mb-4">
-          {aboutData.map((item) => (
+          {aboutData.map((item,itemIndex) => (
             <div
               key={item.title} // Use unique value as key
               className={`${index === itemIndex && 'text-accent after:w-[100%] after:bg-accent after:transition-all after:duration-300'} cursor-pointer capitalize xl:text-lg relative after:w-8 after:h-[2px] after:bg-white after:absolute after:bottom-1 after:left-0`}
@@ -236,9 +236,9 @@ const About = () => {
           ))}
         </div>
         <div className="py-2 xl:py-6 flex flex-col gap-Y-2 xl:gap-Y-4 items-center xl:items-start">
-          {aboutData[index].info.map((item, index) => (
+          {aboutData[index].info.map((item, itemIndex) => (
             <div 
-              key={item.title + index} // Use unique value as key
+              key={item.title + itemIndex} // Use unique value as key
               className="flex-1 flex flex-col md:flex-row max-w-max gap-x-2 items-center text-white/60"
             >
               <div className="font-light">{item.title}</div>
